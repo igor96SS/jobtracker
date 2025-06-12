0523ccfc2cf137101111f13b533d35ab78cffbb8
Igor Simões <igorsimoes_96@hotmail.com>
2025-06-12 17:45:35 +0100

    change company website to not required

--- app/src/main/java/pt/iscode/gestorcandidaturas/entities/Company.kt
+++ app/src/main/java/pt/iscode/gestorcandidaturas/entities/Company.kt
@@ -8,6 +8,6 @@ import androidx.room.PrimaryKey
 data class Company(
     @PrimaryKey(autoGenerate = true) val id:Int = 0,
     @ColumnInfo(name = "name") val name: String,
-    @ColumnInfo(name = "website") val website: String,
+    @ColumnInfo(name = "website") val website: String?,
     @ColumnInfo(name = "linkedin_url") val linkedinUrl: String?
 )
