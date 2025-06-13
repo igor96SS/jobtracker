0dea8922ed32df0392d99b13e49e1b1aa591ab08
Igor Simões <igorsimoes_96@hotmail.com>
2025-06-13 18:59:10 +0100

    add getStatusById to status repository

--- app/src/main/java/pt/iscode/gestorcandidaturas/repositories/StatusRepository.kt
+++ app/src/main/java/pt/iscode/gestorcandidaturas/repositories/StatusRepository.kt
@@ -8,4 +8,8 @@ class StatusRepository(private val statusDao: StatusDAO) {
     suspend fun getAllStatuses(): List<Status>{
         return statusDao.getALL()
     }
+
+    suspend fun getStatusById(id: Int): String{
+        return statusDao.getStatusByID(id)
+    }
 }
