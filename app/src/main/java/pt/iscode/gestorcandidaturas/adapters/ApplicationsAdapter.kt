78818b6077d99b4b9ba873f3c2108f119ab29026
Igor Simões <igorsimoes_96@hotmail.com>
2025-06-20 19:24:15 +0100

    remove unuse import

--- app/src/main/java/pt/iscode/gestorcandidaturas/adapters/ApplicationsAdapter.kt
+++ app/src/main/java/pt/iscode/gestorcandidaturas/adapters/ApplicationsAdapter.kt
@@ -1,6 +1,5 @@
 package pt.iscode.gestorcandidaturas.adapters
 
-import android.content.res.ColorStateList
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
